cb897b3913f143b1481d46cff60b298f586ace13
Eldyn Mohammad Qosim <eldyn.mohammad@gmail.com>
2023-07-06 13:54:51 +0700

    ADD: Axios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "@formkit/nuxt"],
+    modules: ["@nuxtjs/axios", "@nuxtjs/tailwindcss", "@formkit/nuxt"],
     runtimeConfig: {
         public: {
             appUrl: "http://localhost",
